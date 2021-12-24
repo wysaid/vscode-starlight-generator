e3028e9dbcbf827cbe33a1d0481d7db9bc2fc46c
liuyue <liuyue03@kuaishou.com>
2021-12-24 15:31:24 +0800

    - Encode/Decode support find files recursively. - make Encoding url configurable. - Add success notification on status bar.

--- src/coder.js
+++ src/coder.js
@@ -6,66 +6,89 @@ const fs = require('fs');
 const path = require('path');
 const urllib = require('urllib')
 const formstream = require('formstream');
+const { resolve } = require('path');
 
 class Coder {
     inputPath = ''
+    encodingURL = ''
     constructor(inputPath) {
         this.inputPath = inputPath;
+        const config = vscode.workspace.getConfiguration('starlight-generator')
+        this.encodingURL = config.get('encodingURL')
+        this.encodeFile = this.encodeFile.bind(this)
+        this.decodeFile = this.decodeFile.bind(this)
     }
 
-    findFilesWithSuffix(dir, suffix) {
-        let dirCont = fs.readdirSync(dir);
-        let regex = new RegExp('.*\.' + suffix, 'ig')
-        var files = dirCont.filter(function (elm) {
-            return elm.match(regex);
-        });
-        files = files.map(elm => {
-            return path.join(dir, elm)
-        });
+    readDirRecursive = async (filePath, selector) => {
+        const dir = await fs.promises.readdir(filePath);
+        const files = await Promise.all(dir.map(async relativePath => {
+            const absolutePath = path.join(filePath, relativePath);
+            const stat = await fs.promises.lstat(absolutePath);
+            if (stat.isDirectory()) {
+                return this.readDirRecursive(absolutePath, selector);
+            } else {
+                if (selector(absolutePath)) {
+                    return absolutePath;
+                }
+            }
+        }));
+
+        return files.flat().filter(e => e != null);
+    }
+
+    async findFilesWithSuffix(filePath, suffix) {
+        const inputPathStat = fs.statSync(filePath);
+        var files = []
+        if (inputPathStat.isFile()) {
+            files.push(filePath)
+        } else {
+            let regex = new RegExp('.*\.' + suffix + "$", 'i')
+            files = await this.readDirRecursive(filePath, (each) => {
+                return each.match(regex) != null;
+            })
+        }
         return files
     }
 
-    // curl -F "file=@$fileName" -F "op=decode"
+    // curl -F "file=@$fileName" -F "op=encode"
     // http://mac.yyyyy.tech/faceDetect/encode.php
-    encode() {
-        const inputPathStat = fs.statSync(this.inputPath);
-        var files = []
-        if (inputPathStat.isFile()) {
-            files.push(this.inputPath)
-        }
-        else {
-            const filesNeedEncode = this.findFilesWithSuffix(this.inputPath, 'lua')
-            files = [].concat(filesNeedEncode)
-        }
-
+    async encode() {
+        const files = await this.findFilesWithSuffix(this.inputPath, "lua")
         if (files.length == 0) {
             vscode.window.showErrorMessage("Encode failed: no files");
             return;
         }
 
-        files.forEach(elm => { this.sendEncodeRequest(elm) })
+        const encodingDisposable = vscode.window.setStatusBarMessage("Encoding Files ...", 5000)
+        Promise
+            .all(files.map(this.encodeFile))
+            .then(ret => {
+                vscode.window.setStatusBarMessage("Encoding Files Done.", 1000)
+                encodingDisposable.dispose()
+                console.log("Encoding Files Done => \r\n" + ret.join('\r\n'))
+            })
     }
 
-    decode() {
-        const inputPathStat = fs.statSync(this.inputPath);
-        var files = []
-        if (inputPathStat.isFile()) {
-            files.push(this.inputPath)
-        }
-        else {
-            const filesNeedDecode = this.findFilesWithSuffix(this.inputPath, 'ex')
-            files = [].concat(filesNeedDecode)
-        }
-
+    // curl -F "file=@$fileName" -F "op=decode"
+    // http://mac.yyyyy.tech/faceDetect/encode.php
+    async decode() {
+        const files = await this.findFilesWithSuffix(this.inputPath, "ex")
         if (files.length == 0) {
             vscode.window.showErrorMessage("Decode failed: no files");
             return;
         }
 
-        files.forEach(elm => { this.sendDecodeRequest(elm) })
+        const decodingDisposable = vscode.window.setStatusBarMessage("Decoding Files ...", 5000)
+        Promise
+            .all(files.map(this.decodeFile))
+            .then(ret => {
+                vscode.window.setStatusBarMessage("Decoding Files Done.", 1000)
+                decodingDisposable.dispose()
+                console.log("Decoding Files Done => \r\n" + ret.join('\r\n'))
+            })
     }
 
-    sendEncodeRequest(filePath) {
+    async encodeFile(filePath) {
         const dirname = path.dirname(filePath)
         const fileBasename = path.basename(filePath, '.lua')
         const filename = path.basename(filePath)
@@ -74,15 +97,22 @@ class Coder {
         form.buffer('file', fs.readFileSync(filePath), filename, 'application/octet-stream')
         form.field('op', 'encode')
 
-        urllib.request(
-            'http://mac.yyyyy.tech/faceDetect/encode.php',
-            { method: 'POST', headers: form.headers(), stream: form },
-            function (err, data, res) {
-                fs.writeFileSync(outputFilePath, data)
-            });
+        return urllib
+            .request(
+                this.encodingURL,
+                {
+                    method: 'POST',
+                    headers: form.headers(),
+                    stream: form
+                }
+            )
+            .then(function (result) {
+                fs.writeFileSync(outputFilePath, result.data)
+                return outputFilePath
+            })
     }
 
-    sendDecodeRequest(filePath) {
+    async decodeFile(filePath) {
         const config = vscode.workspace.getConfiguration('starlight-generator')
         const dstSuffix = config.get("decodedFileSuffix")
 
@@ -94,12 +124,18 @@ class Coder {
         form.buffer('file', fs.readFileSync(filePath), filename, 'application/octet-stream')
         form.field('op', 'decode')
 
-        urllib.request(
-            'http://mac.yyyyy.tech/faceDetect/encode.php',
-            { method: 'POST', headers: form.headers(), stream: form },
-            function (err, data, res) {
-                fs.writeFileSync(outputFilePath, data)
-            });
+        return urllib.
+            request(
+                this.encodingURL,
+                {
+                    method: 'POST',
+                    headers: form.headers(),
+                    stream: form
+                })
+            .then(function (result) {
+                fs.writeFileSync(outputFilePath, result.data)
+                return outputFilePath
+            })
     }
 }
 
